2bcd831e870e95bbf102bacd932722fa897c1b3f
normanlys <limyueshing@gmail.com>
2019-05-05 19:42:20 +0800

    Add MuscleGroup enum

---
+++ WorkoutTracker/WorkoutTracker/Models/MuscleGroup.swift
@@ -0,0 +1,29 @@
+//
+//  MuscleGroup.swift
+//  WorkoutTracker
+//
+//  Created by Norman Lim on 5/5/2019.
+//  Copyright © 2019 Norman Lim. All rights reserved.
+//
+
+import Foundation
+
+enum MuscleGroup: String {
+    case chest
+    case shoulder
+    case abdominal
+    
+    // arms
+    case triceps
+    case biceps
+//    case forearms
+    
+    // legs
+    case quadriceps
+    case hamstrings
+    case calves
+    
+    // back
+    case back
+    case traps
+}
