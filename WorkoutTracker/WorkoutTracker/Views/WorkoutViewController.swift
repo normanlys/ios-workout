eb3459fb52df3cfa90426f0e5b26040b1751aef5
normanlys <limyueshing@gmail.com>
2019-05-08 20:32:28 +0800

    Create WorkoutViewController.swift

---
+++ WorkoutTracker/WorkoutTracker/Views/WorkoutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExercisesViewController.swift
+//  WorkoutTracker
+//
+//  Created by Norman Lim on 8/5/2019.
+//  Copyright © 2019 Norman Lim. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
