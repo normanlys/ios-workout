80fb28e6a5f1f42c946ec203a828a5713478dcd8
Norman <limyueshing@gmail.com>
2019-09-04 23:05:01 +0800

    Add UIColor extensions

---
+++ WorkoutTracker/WorkoutTracker/Extensions/ColorExtensions.swift
@@ -0,0 +1,40 @@
+//
+//  ColorExtensions.swift
+//  WorkoutTracker
+//
+//  Created by Norman Lim on 4/9/2019.
+//  Copyright © 2019 Norman Lim. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init?(r: Int, g: Int, b: Int) {
+        let range = 0...255
+        guard range ~= r, range ~= g, range ~= b else { return nil }
+        
+        self.init(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init?(hexString: String) {
+        var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
+        let red, green, blue, alpha: CGFloat
+        switch chars.count {
+        case 3:
+            chars = chars.flatMap { [$0, $0] }
+            fallthrough
+        case 6:
+            chars = ["F","F"] + chars
+            fallthrough
+        case 8:
+            alpha = CGFloat(strtoul(String(chars[0...1]), nil, 16)) / 255
+            red   = CGFloat(strtoul(String(chars[2...3]), nil, 16)) / 255
+            green = CGFloat(strtoul(String(chars[4...5]), nil, 16)) / 255
+            blue  = CGFloat(strtoul(String(chars[6...7]), nil, 16)) / 255
+        default:
+            return nil
+        }
+        self.init(red: red, green: green, blue:  blue, alpha: alpha)
+    }
+}
